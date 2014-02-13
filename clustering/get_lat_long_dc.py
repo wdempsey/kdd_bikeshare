111c828a35941622fb8d9c26b411be85014272fa
Jette Henderson <jette.henderson@gmail.com>
2014-02-13 10:52:33 -0600

    added clustering folder and code to extract metadata from database

---
+++ clustering/get_lat_long_dc.py
@@ -0,0 +1,17 @@
+import os
+import psycopg2
+
+conn = psycopg2.connect(database='bikeshare',user='bikeshare',host='bikeshare.ctmvy2bluoic.us-west-2.rds.amazonaws.com',password="bikeshare")
+
+cur = conn.cursor() 
+
+# get all the DC metadata
+cur.execute("SELECT * FROM metadata_washingtondc")
+metadata = cur.fetchall()
+
+cur.execute("SELECT table_name FROM information_schema.tables")
+tables = cur.fetchall()
+
+# the following two commands get all the table names in the database
+for i in range(0,len(metadata)):
+    print str(metadata[i])[1:-1]
