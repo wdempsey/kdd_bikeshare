e007b1cc842dd4b68650c59c630c2ca606eff992
Jette Henderson <jette.henderson@gmail.com>
2014-02-13 10:56:47 -0600

    moved a comment to be in the right place

--- clustering/get_lat_long_dc.py
+++ clustering/get_lat_long_dc.py
@@ -9,9 +9,9 @@ cur = conn.cursor()
 cur.execute("SELECT * FROM metadata_washingtondc")
 metadata = cur.fetchall()
 
+# the following two commands get all the table names in the database
 cur.execute("SELECT table_name FROM information_schema.tables")
 tables = cur.fetchall()
 
-# the following two commands get all the table names in the database
 for i in range(0,len(metadata)):
     print str(metadata[i])[1:-1]
